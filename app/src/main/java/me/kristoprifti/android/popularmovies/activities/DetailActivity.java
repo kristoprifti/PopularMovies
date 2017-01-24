7a479ae9ce4857e3fa0eed15b9cb2d39280749af
Kristo Prifti <kristopriti93@gmail.com>
2017-01-24 17:45:42 +0200

    added functionaliy for add to favorites

--- app/src/main/java/me/kristoprifti/android/popularmovies/activities/DetailActivity.java
+++ app/src/main/java/me/kristoprifti/android/popularmovies/activities/DetailActivity.java
@@ -1,19 +1,25 @@
 package me.kristoprifti.android.popularmovies.activities;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
@@ -21,6 +27,7 @@ import com.squareup.picasso.Picasso;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import me.kristoprifti.android.popularmovies.R;
+import me.kristoprifti.android.popularmovies.data.MoviesContract;
 import me.kristoprifti.android.popularmovies.models.Movie;
 
 public class DetailActivity extends AppCompatActivity {
@@ -39,8 +46,7 @@ public class DetailActivity extends AppCompatActivity {
     @BindView(R.id.iv_movie_poster) ImageView mMoviePosterImageView;
     @BindView(R.id.appbar) AppBarLayout appBarLayout;
     @BindView(R.id.collapsing_toolbar) CollapsingToolbarLayout collapsingToolbarLayout;
-
-    private Movie mMovie;
+    @BindView(R.id.favoriteButton) FloatingActionButton addToFavorites;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,34 +70,73 @@ public class DetailActivity extends AppCompatActivity {
             colorizeActivity(colorPalette);
 
             if (intent.hasExtra(getString(R.string.intent_movie_object))) {
-                mMovie = intent.getParcelableExtra(getString(R.string.intent_movie_object));
+                final Movie movie = intent.getParcelableExtra(getString(R.string.intent_movie_object));
 
                 if(getSupportActionBar() != null) {
-                    getSupportActionBar().setTitle(mMovie.getOriginalTitle());
+                    getSupportActionBar().setTitle(movie.getOriginalTitle());
                 }
 
-                mMovieFulltitleTextView.setText(mMovie.getOriginalTitle());
-                mMovieReleaseDateTextView.append(mMovie.getReleaseDate());
-                mMovieOriginalLanguageTextView.append(mMovie.getOriginalLanguage());
-                mMoviePopularityTextView.append(String.valueOf(mMovie.getPopularity()));
-                mMovieVotesTextView.append(String.valueOf(mMovie.getVoteCount()));
-                mMovieRatingTextView.append(String.valueOf(mMovie.getRating()) + getString(R.string.out_of_ten));
-                mMovieOverviewTextView.setText(mMovie.getOverview());
-                Picasso.with(this).load(mMovie.getBackdropPath()).into(mMovieBackdropImageView);
-                Picasso.with(this).load(mMovie.getPosterPath()).into(mMoviePosterImageView, new Callback() {
-                    @Override
-                    public void onSuccess() {
-                        ActivityCompat.startPostponedEnterTransition(DetailActivity.this);
-                    }
+                displayMovieData(movie);
 
+                addToFavorites.setOnClickListener(new View.OnClickListener() {
                     @Override
-                    public void onError() {
-
+                    public void onClick(View v) {
+                        addMovieToFavorites(movie);
                     }
                 });
-
             }
         }
+    }
+
+    private void addMovieToFavorites(final Movie movie) {
+        new AsyncTask<Void, Void, Uri>() {
+            @Override
+            protected Uri doInBackground(Void... params) {
+                ContentValues values = new ContentValues();
+
+                values.put(MoviesContract.MoviesEntry.COLUMN_MOVIE_ID, movie.getMovieId());
+                values.put(MoviesContract.MoviesEntry.COLUMN_MOVIE_TITLE, movie.getOriginalTitle());
+                values.put(MoviesContract.MoviesEntry.COLUMN_MOVIE_RATING, movie.getRating());
+                values.put(MoviesContract.MoviesEntry.COLUMN_MOVIE_BACKDROP, movie.getBackdropPath());
+                values.put(MoviesContract.MoviesEntry.COLUMN_MOVIE_POSTER, movie.getPosterPath());
+                values.put(MoviesContract.MoviesEntry.COLUMN_MOVIE_OVERVIEW, movie.getOverview());
+                values.put(MoviesContract.MoviesEntry.COLUMN_MOVIE_POPULARITY, movie.getPopularity());
+                values.put(MoviesContract.MoviesEntry.COLUMN_MOVIE_VOTES, movie.getVoteCount());
+                values.put(MoviesContract.MoviesEntry.COLUMN_MOVIE_RELEASE_DATE, movie.getReleaseDate());
+                values.put(MoviesContract.MoviesEntry.COLUMN_MOVIE_LANGUAGE, movie.getOriginalLanguage());
+
+                return getContentResolver().insert(MoviesContract.MoviesEntry.CONTENT_URI,
+                        values);
+            }
+
+            @Override
+            protected void onPostExecute(Uri returnUri) {
+                //add.setIcon(R.drawable.abc_btn_rating_star_on_mtrl_alpha);
+                Toast.makeText(DetailActivity.this, "Added to favorites", Toast.LENGTH_SHORT).show();
+            }
+        }.execute();
+    }
+
+    private void displayMovieData(Movie movie) {
+        mMovieFulltitleTextView.setText(movie.getOriginalTitle());
+        mMovieReleaseDateTextView.append(movie.getReleaseDate());
+        mMovieOriginalLanguageTextView.append(movie.getOriginalLanguage());
+        mMoviePopularityTextView.append(String.valueOf(movie.getPopularity()));
+        mMovieVotesTextView.append(String.valueOf(movie.getVoteCount()));
+        mMovieRatingTextView.append(String.valueOf(movie.getRating()) + getString(R.string.out_of_ten));
+        mMovieOverviewTextView.setText(movie.getOverview());
+        Picasso.with(this).load(movie.getBackdropPath()).into(mMovieBackdropImageView);
+        Picasso.with(this).load(movie.getPosterPath()).into(mMoviePosterImageView, new Callback() {
+            @Override
+            public void onSuccess() {
+                ActivityCompat.startPostponedEnterTransition(DetailActivity.this);
+            }
+
+            @Override
+            public void onError() {
+
+            }
+        });
     }
 
     //get color from Palette API
@@ -123,7 +168,7 @@ public class DetailActivity extends AppCompatActivity {
     private Intent createShareForecastIntent() {
         return ShareCompat.IntentBuilder.from(this)
                 .setType("text/plain")
-                .setText(MOVIE_SHARE_HASHTAG + getString(R.string.hashTag) + mMovie.getOriginalTitle())
+                .setText(MOVIE_SHARE_HASHTAG + getString(R.string.hashTag) + mMovieFulltitleTextView.getText().toString())
                 .getIntent();
     }
 
